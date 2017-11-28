c97deb6f6c768a072ed86a68e7a03222a9b33a46
Sofia <sofia.zakladnaya@gmail.com>
2017-11-28 22:51:00 +0300

    Добавлена проверка корректности параметров

--- integrate_multithread/integrate_multithread/Form1.Designer.cs
+++ integrate_multithread/integrate_multithread/Form1.Designer.cs
@@ -32,27 +32,27 @@
             this.справкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.оПрограммеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.pnSettings = new System.Windows.Forms.Panel();
-            this.gbIntMethod = new System.Windows.Forms.GroupBox();
-            this.rbRectangle = new System.Windows.Forms.RadioButton();
-            this.rbTrapezoidal = new System.Windows.Forms.RadioButton();
-            this.rbSimpson = new System.Windows.Forms.RadioButton();
-            this.label1 = new System.Windows.Forms.Label();
-            this.tbUpperLimit = new System.Windows.Forms.TextBox();
-            this.tbLowerLimit = new System.Windows.Forms.TextBox();
-            this.lbFunction = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.btnSolve = new System.Windows.Forms.Button();
+            this.tbThreadsNum = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbInvervalsNum = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.btnClearResults = new System.Windows.Forms.Button();
+            this.btnSolve = new System.Windows.Forms.Button();
+            this.cbFunction = new System.Windows.Forms.ComboBox();
+            this.lbFunction = new System.Windows.Forms.Label();
+            this.tbLowerLimit = new System.Windows.Forms.TextBox();
+            this.tbUpperLimit = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gbIntMethod = new System.Windows.Forms.GroupBox();
+            this.rbSimpson = new System.Windows.Forms.RadioButton();
+            this.rbTrapezoidal = new System.Windows.Forms.RadioButton();
+            this.rbRectangle = new System.Windows.Forms.RadioButton();
             this.dgResults = new System.Windows.Forms.DataGridView();
             this.IntegralValue = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.IntervalNumber = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ThreadNumber = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Time = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Acceleration = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.label2 = new System.Windows.Forms.Label();
-            this.tbInvervalsNum = new System.Windows.Forms.TextBox();
-            this.tbThreadsNum = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
             this.pnSettings.SuspendLayout();
             this.gbIntMethod.SuspendLayout();
@@ -80,7 +80,7 @@
             // оПрограммеToolStripMenuItem
             // 
             this.оПрограммеToolStripMenuItem.Name = "оПрограммеToolStripMenuItem";
-            this.оПрограммеToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.оПрограммеToolStripMenuItem.Size = new System.Drawing.Size(149, 22);
             this.оПрограммеToolStripMenuItem.Text = "О программе";
             this.оПрограммеToolStripMenuItem.Click += new System.EventHandler(this.оПрограммеToolStripMenuItem_Click);
             // 
@@ -92,7 +92,7 @@
             this.pnSettings.Controls.Add(this.label2);
             this.pnSettings.Controls.Add(this.btnClearResults);
             this.pnSettings.Controls.Add(this.btnSolve);
-            this.pnSettings.Controls.Add(this.comboBox1);
+            this.pnSettings.Controls.Add(this.cbFunction);
             this.pnSettings.Controls.Add(this.lbFunction);
             this.pnSettings.Controls.Add(this.tbLowerLimit);
             this.pnSettings.Controls.Add(this.tbUpperLimit);
@@ -102,6 +102,101 @@
             this.pnSettings.Name = "pnSettings";
             this.pnSettings.Size = new System.Drawing.Size(200, 466);
             this.pnSettings.TabIndex = 2;
+            // 
+            // tbThreadsNum
+            // 
+            this.tbThreadsNum.Location = new System.Drawing.Point(3, 274);
+            this.tbThreadsNum.Name = "tbThreadsNum";
+            this.tbThreadsNum.Size = new System.Drawing.Size(188, 20);
+            this.tbThreadsNum.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 258);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Число потоков";
+            // 
+            // tbInvervalsNum
+            // 
+            this.tbInvervalsNum.Location = new System.Drawing.Point(3, 224);
+            this.tbInvervalsNum.Name = "tbInvervalsNum";
+            this.tbInvervalsNum.Size = new System.Drawing.Size(188, 20);
+            this.tbInvervalsNum.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 208);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Число разбиений";
+            // 
+            // btnClearResults
+            // 
+            this.btnClearResults.Location = new System.Drawing.Point(116, 425);
+            this.btnClearResults.Name = "btnClearResults";
+            this.btnClearResults.Size = new System.Drawing.Size(75, 28);
+            this.btnClearResults.TabIndex = 7;
+            this.btnClearResults.Text = "Очистить";
+            this.btnClearResults.UseVisualStyleBackColor = true;
+            this.btnClearResults.Click += new System.EventHandler(this.btnClearResults_Click);
+            // 
+            // btnSolve
+            // 
+            this.btnSolve.Location = new System.Drawing.Point(3, 425);
+            this.btnSolve.Name = "btnSolve";
+            this.btnSolve.Size = new System.Drawing.Size(75, 28);
+            this.btnSolve.TabIndex = 6;
+            this.btnSolve.Text = "Вычислить";
+            this.btnSolve.UseVisualStyleBackColor = true;
+            this.btnSolve.Click += new System.EventHandler(this.btnSolve_Click);
+            // 
+            // cbFunction
+            // 
+            this.cbFunction.FormattingEnabled = true;
+            this.cbFunction.Items.AddRange(new object[] {
+            "sin(x)",
+            "e^x"});
+            this.cbFunction.Location = new System.Drawing.Point(3, 25);
+            this.cbFunction.Name = "cbFunction";
+            this.cbFunction.Size = new System.Drawing.Size(188, 21);
+            this.cbFunction.TabIndex = 5;
+            // 
+            // lbFunction
+            // 
+            this.lbFunction.AutoSize = true;
+            this.lbFunction.Location = new System.Drawing.Point(6, 9);
+            this.lbFunction.Name = "lbFunction";
+            this.lbFunction.Size = new System.Drawing.Size(53, 13);
+            this.lbFunction.TabIndex = 4;
+            this.lbFunction.Text = "Функция";
+            // 
+            // tbLowerLimit
+            // 
+            this.tbLowerLimit.Location = new System.Drawing.Point(3, 72);
+            this.tbLowerLimit.Name = "tbLowerLimit";
+            this.tbLowerLimit.Size = new System.Drawing.Size(45, 20);
+            this.tbLowerLimit.TabIndex = 3;
+            // 
+            // tbUpperLimit
+            // 
+            this.tbUpperLimit.Location = new System.Drawing.Point(75, 72);
+            this.tbUpperLimit.Name = "tbUpperLimit";
+            this.tbUpperLimit.Size = new System.Drawing.Size(45, 20);
+            this.tbUpperLimit.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Отрезок интегрирования";
             // 
             // gbIntMethod
             // 
@@ -115,16 +210,16 @@
             this.gbIntMethod.TabStop = false;
             this.gbIntMethod.Text = "Метод";
             // 
-            // rbRectangle
+            // rbSimpson
             // 
-            this.rbRectangle.AutoSize = true;
-            this.rbRectangle.Location = new System.Drawing.Point(6, 19);
-            this.rbRectangle.Name = "rbRectangle";
-            this.rbRectangle.Size = new System.Drawing.Size(117, 17);
-            this.rbRectangle.TabIndex = 0;
-            this.rbRectangle.TabStop = true;
-            this.rbRectangle.Text = "Прямоугольников";
-            this.rbRectangle.UseVisualStyleBackColor = true;
+            this.rbSimpson.AutoSize = true;
+            this.rbSimpson.Location = new System.Drawing.Point(6, 65);
+            this.rbSimpson.Name = "rbSimpson";
+            this.rbSimpson.Size = new System.Drawing.Size(76, 17);
+            this.rbSimpson.TabIndex = 2;
+            this.rbSimpson.TabStop = true;
+            this.rbSimpson.Text = "Симпсона";
+            this.rbSimpson.UseVisualStyleBackColor = true;
             // 
             // rbTrapezoidal
             // 
@@ -137,75 +232,16 @@
             this.rbTrapezoidal.Text = "Трапеций";
             this.rbTrapezoidal.UseVisualStyleBackColor = true;
             // 
-            // rbSimpson
+            // rbRectangle
             // 
-            this.rbSimpson.AutoSize = true;
-            this.rbSimpson.Location = new System.Drawing.Point(6, 65);
-            this.rbSimpson.Name = "rbSimpson";
-            this.rbSimpson.Size = new System.Drawing.Size(76, 17);
-            this.rbSimpson.TabIndex = 2;
-            this.rbSimpson.TabStop = true;
-            this.rbSimpson.Text = "Симпсона";
-            this.rbSimpson.UseVisualStyleBackColor = true;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 56);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(135, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Отрезок интегрирования";
-            // 
-            // tbUpperLimit
-            // 
-            this.tbUpperLimit.Location = new System.Drawing.Point(75, 72);
-            this.tbUpperLimit.Name = "tbUpperLimit";
-            this.tbUpperLimit.Size = new System.Drawing.Size(45, 20);
-            this.tbUpperLimit.TabIndex = 2;
-            // 
-            // tbLowerLimit
-            // 
-            this.tbLowerLimit.Location = new System.Drawing.Point(3, 72);
-            this.tbLowerLimit.Name = "tbLowerLimit";
-            this.tbLowerLimit.Size = new System.Drawing.Size(45, 20);
-            this.tbLowerLimit.TabIndex = 3;
-            // 
-            // lbFunction
-            // 
-            this.lbFunction.AutoSize = true;
-            this.lbFunction.Location = new System.Drawing.Point(6, 9);
-            this.lbFunction.Name = "lbFunction";
-            this.lbFunction.Size = new System.Drawing.Size(53, 13);
-            this.lbFunction.TabIndex = 4;
-            this.lbFunction.Text = "Функция";
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(3, 25);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(188, 21);
-            this.comboBox1.TabIndex = 5;
-            // 
-            // btnSolve
-            // 
-            this.btnSolve.Location = new System.Drawing.Point(3, 425);
-            this.btnSolve.Name = "btnSolve";
-            this.btnSolve.Size = new System.Drawing.Size(75, 28);
-            this.btnSolve.TabIndex = 6;
-            this.btnSolve.Text = "Вычислить";
-            this.btnSolve.UseVisualStyleBackColor = true;
-            // 
-            // btnClearResults
-            // 
-            this.btnClearResults.Location = new System.Drawing.Point(116, 425);
-            this.btnClearResults.Name = "btnClearResults";
-            this.btnClearResults.Size = new System.Drawing.Size(75, 28);
-            this.btnClearResults.TabIndex = 7;
-            this.btnClearResults.Text = "Очистить";
-            this.btnClearResults.UseVisualStyleBackColor = true;
-            this.btnClearResults.Click += new System.EventHandler(this.btnClearResults_Click);
+            this.rbRectangle.AutoSize = true;
+            this.rbRectangle.Location = new System.Drawing.Point(6, 19);
+            this.rbRectangle.Name = "rbRectangle";
+            this.rbRectangle.Size = new System.Drawing.Size(117, 17);
+            this.rbRectangle.TabIndex = 0;
+            this.rbRectangle.TabStop = true;
+            this.rbRectangle.Text = "Прямоугольников";
+            this.rbRectangle.UseVisualStyleBackColor = true;
             // 
             // dgResults
             // 
@@ -228,58 +264,31 @@
             // 
             this.IntegralValue.HeaderText = "Значение интеграла";
             this.IntegralValue.Name = "IntegralValue";
+            this.IntegralValue.ReadOnly = true;
             // 
             // IntervalNumber
             // 
             this.IntervalNumber.HeaderText = "Число разбиений";
             this.IntervalNumber.Name = "IntervalNumber";
+            this.IntervalNumber.ReadOnly = true;
             // 
             // ThreadNumber
             // 
             this.ThreadNumber.HeaderText = "Число потоков";
             this.ThreadNumber.Name = "ThreadNumber";
+            this.ThreadNumber.ReadOnly = true;
             // 
             // Time
             // 
             this.Time.HeaderText = "Время, мс";
             this.Time.Name = "Time";
+            this.Time.ReadOnly = true;
             // 
             // Acceleration
             // 
             this.Acceleration.HeaderText = "Ускорение";
             this.Acceleration.Name = "Acceleration";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(6, 208);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(96, 13);
-            this.label2.TabIndex = 8;
-            this.label2.Text = "Число разбиений";
-            // 
-            // tbInvervalsNum
-            // 
-            this.tbInvervalsNum.Location = new System.Drawing.Point(3, 224);
-            this.tbInvervalsNum.Name = "tbInvervalsNum";
-            this.tbInvervalsNum.Size = new System.Drawing.Size(188, 20);
-            this.tbInvervalsNum.TabIndex = 9;
-            // 
-            // tbThreadsNum
-            // 
-            this.tbThreadsNum.Location = new System.Drawing.Point(3, 274);
-            this.tbThreadsNum.Name = "tbThreadsNum";
-            this.tbThreadsNum.Size = new System.Drawing.Size(188, 20);
-            this.tbThreadsNum.TabIndex = 11;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(6, 258);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(83, 13);
-            this.label3.TabIndex = 10;
-            this.label3.Text = "Число потоков";
+            this.Acceleration.ReadOnly = true;
             // 
             // Form1
             // 
@@ -315,7 +324,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button btnClearResults;
         private System.Windows.Forms.Button btnSolve;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox cbFunction;
         private System.Windows.Forms.Label lbFunction;
         private System.Windows.Forms.TextBox tbLowerLimit;
         private System.Windows.Forms.TextBox tbUpperLimit;
--- integrate_multithread/integrate_multithread/Form1.cs
+++ integrate_multithread/integrate_multithread/Form1.cs
@@ -28,5 +28,71 @@ namespace integrate_multithread
             //Очищаем таблицу результатов
             dgResults.Rows.Clear();
         }
+
+        private void btnSolve_Click(object sender, EventArgs e)
+        {
+            //Проверяем корректность параметров
+            try
+            {
+                Verify();
+            }
+            catch(FormatException ex0)
+            {
+                MessageBox.Show("Вводимые параметры должны быть числами");
+                return;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return;
+            }
+
+            //Если параметры заданы корректно, запускаем решение
+           
+            
+        }
+
+        public int Verify()
+        {
+            double a, b;
+            int n, k;
+           
+           
+
+            //проверяем, выбрана ли подынтегральная функция
+            if (cbFunction.SelectedIndex < 0)
+            {
+                throw new Exception("Не выбрана подынтегральная функция");
+            }
+
+            a = Convert.ToDouble(tbLowerLimit.Text);
+            b = Convert.ToDouble(tbUpperLimit.Text);
+           
+
+            //проверяем отрезок интегрирования
+            if (a>b)
+            {
+                throw new Exception("Отрезок интегрирования должен быть не вырожденным");
+            }
+
+            n = Convert.ToInt32(tbInvervalsNum.Text);
+            
+            //проверяем число разбиений
+            if (n<=0)
+            {
+                throw new Exception("Количество разбиений должнобыть положительным числом");
+            }
+
+            k = Convert.ToInt32(tbThreadsNum.Text);
+            //проверяем число потоков
+            if (k <= 0)
+            {
+                throw new Exception("Количество потоков должнобыть положительным числом");
+            }   
+            
+            //Если всё заданно корректно
+            return 0;
+        }
+
     }
 }
