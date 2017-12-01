c00b323e00694d5a5d7b408249bf16d9de3ea7e5
Sofia <sofia.zakladnaya@gmail.com>
2017-12-01 13:06:32 +0300

    Merge branch 'thread_and_time' of https://github.com/sofia-zakladnaya/multithread into thread_and_time
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

---
+++ integrate_multithread/integrate_multithread/Threader.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace integrate_multithread
+{
+    //Распределение задачи по потокам, сбор результатов, подсчёт времени и ускорения
+    class Threader
+    {
+        //Список потоков
+        public List<Thread> Threads { get; set; }
+        //Время выполнения
+        public TimeSpan TotalTime { get; set; }
+
+        //Распределение интервалов по потокам
+        public void Distribute(Integral I, int threadscount)
+        {
+            //Инициализируем список потоков
+            Threads = new List<Thread>();
+            //Делим отрезок интегрирования между потоками
+            List<Integral> SubIntegrals = new List<Integral>();
+            double h = (I.UpperLimit - I.LowerLimit) / threadscount;
+            for (int i=0;i<threadscount;i++)
+            {
+                Integral intg = I;
+                //Изменяем пределы и точность
+                intg.Eps /= threadscount;
+                intg.LowerLimit = I.LowerLimit + i*h;
+                intg.UpperLimit = I.LowerLimit + (i + 1) * h;
+                //Включаем интеграл в список
+                SubIntegrals.Add(intg);
+
+                //Создаём поток и добавляем в список
+                ThreadStart solver = new ThreadStart(SubIntegrals[i].Solve);
+                Thread thread = new Thread(solver);
+                Threads.Add(thread);
+            }
+
+        }
+
+        //Вычисление ускорения
+        public double Acceleration(TimeSpan onethread)
+        {
+            return (onethread.TotalMilliseconds /TotalTime.TotalMilliseconds);
+        }
+    }
+
+    //public delegate double Function(double x);
+
+}
